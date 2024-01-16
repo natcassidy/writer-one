179590bef675f47597cea464e6a39c97b9816ce0
natcassidy <nat.cassidy865@gmail.com>
2024-01-15 22:21:42 -0400

    Updated the comment for processBlogPost to provide the body

--- functions/routes/aiRoutes.js
+++ functions/routes/aiRoutes.js
@@ -5,8 +5,36 @@ router.get('/health', (req, res) => {
     res.status(200).send("I'm alive")
 })
 
+/*
+
+Here's the body to expect from the client
+
+{
+  "mainKeyword": "String",
+  "title": "String",
+  "articleLength": {
+    "type": "String",
+    "enum": ["small", "medium", "large", "xLarge"]
+  },
+  "writingStyle": {
+    "type": "String",
+    "enum": ["light", "professional"]
+  },
+  "pointOfView": {
+    "type": "String",
+    "enum": ["firstPersonSingular", "firstPersonPlural", "thirdPerson", "neutral"]
+  },
+  "realTimeResearch": "Boolean",
+  "citeSources": "Boolean",
+  "includeFAQs": "Boolean",
+  "generateImages": "Boolean",
+  "internalUrl": "String"
+}
+
+*/
 router.post('/processBlogPost', (req, res) => {
   res.status(200).send("Processed Blog Post")
 });
 
 module.exports = router;
+
