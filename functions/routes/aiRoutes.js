e9f4777f62c4a773a513820a18eb0661c4dafc9c
natcassidy <nat.cassidy865@gmail.com>
2024-01-28 22:40:32 -0400

    Added openai config

--- functions/routes/aiRoutes.js
+++ functions/routes/aiRoutes.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const OpenAI = require("openai");
 
-router.get('/health', (req, res) => {
-    res.status(200).send("I'm alive ai")
+const openai = new OpenAI({
+  apiKey: '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',
+});
+
+router.get('/health', async (req, res) => {
+
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant designed to output JSON.",
+        },
+        { role: "user", content: "Who won the world series in 2020?" },
+      ],
+      model: "gpt-3.5-turbo-1106",
+      response_format: { type: "json_object" },
+    });
+  } catch (e) {
+    console.log('exception:, ', e)
+  }
+
+
+  res.status(200).send(completion.choices[0].message.content)
 })
 
 /*
